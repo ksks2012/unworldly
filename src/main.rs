a4377b2daebfb66b50d583971a3f5f79489b9abc
ksks2012 <st890751999@gmail.com>
2024-01-26 22:26:45 +0800

    feat(basic): Add basic components
    - Position
    - Renderable
    - Name
    - Biotechnology

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use rltk::{GameState, Rltk};
+use specs::prelude::*;
+
+mod components;
+pub use components::*;
+mod biotechnology;
+mod map;
+pub use map::*;
+
+pub struct State {
+    pub ecs: World
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+
+        self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let mut context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    context.with_post_scanlines(true);
+    let mut gs: State = State {
+        ecs: World::new(),
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Biotechnology>();
+    gs.ecs.register::<Name>();
+
+    gs.ecs.insert(new_map());
+
+    rltk::main_loop(context, gs)
+}
