3ded9057d79760b110d23681c392faf2b659edb5
ksks2012 <st890751999@gmail.com>
2024-05-25 17:05:41 +0800

    refactor(about): Change folder layer of about

--- crates/egui_demo_app/src/lib.rs
+++ crates/egui_demo_app/src/lib.rs
@@ -3,10 +3,10 @@
 
 mod wrap_app;
 mod ui;
-mod about;
+mod demo;
 
 pub use {
-    about::About, ui::DemoWindows, wrap_app::WrapApp,
+    ui::DemoWindows, wrap_app::WrapApp, demo::MiscDemoWindow,
 };
 
 // ----------------------------------------------------------------------------
@@ -16,22 +16,3 @@ mod web;
 
 #[cfg(target_arch = "wasm32")]
 pub use web::*;
-
-/// Something to view in the demo windows
-pub trait View {
-    fn ui(&mut self, ui: &mut egui::Ui);
-}
-
-/// Something to view
-pub trait Demo {
-    /// Is the demo enabled for this integration?
-    fn is_enabled(&self, _ctx: &egui::Context) -> bool {
-        true
-    }
-
-    /// `&'static` so we can also use it as a key to store open/close state.
-    fn name(&self) -> &'static str;
-
-    /// Show windows, etc
-    fn show(&mut self, ctx: &egui::Context, open: &mut bool);
-}
